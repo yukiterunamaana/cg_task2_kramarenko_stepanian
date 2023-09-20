f8bef2b50cba028bea57f2cc5357e91f8e857b41
yukiterunamaana <74673274+yukiterunamaana@users.noreply.github.com>
2023-09-20 14:23:06 +0300

    Update Program.cs

--- CG_Task2_Kramarenko_Stepanian/CG_Task2_Kramarenko_Stepanian/Program.cs
+++ CG_Task2_Kramarenko_Stepanian/CG_Task2_Kramarenko_Stepanian/Program.cs
@@ -1,22 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace CG_Task2_Kramarenko_Stepanian
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
+        static void Task1() 
+        {
+            Bitmap image = new Bitmap("input1.jpg");
+            for (int x = 0; x < image.Width; x++)
+            for (int y = 0; y < image.Height; y++)
+            {
+                Color pixel = image.GetPixel(x, y);
+                int grayValue = (int)(0.299 * pixel.R + 0.587 * pixel.G + 0.114 * pixel.B);
+                Color newPixel = Color.FromArgb(grayValue, grayValue, grayValue);
+                image.SetPixel(x, y, newPixel);
+            }
+            image.Save("output1.jpg");
+        }
+
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Task1();
         }
     }
 }
